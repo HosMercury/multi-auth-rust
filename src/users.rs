a5af15b770fd5a3d66daeb36d6d6c2a93dbeafa5
HosMercury <hos.mercury@gmail.com>
2024-02-07 01:30:53 +0200

    firat

---
+++ src/users.rs
@@ -0,0 +1,238 @@
+use async_trait::async_trait;
+use axum::http::header::{AUTHORIZATION, USER_AGENT};
+use axum_login::{AuthUser, AuthnBackend, UserId};
+use oauth2::{
+    basic::{BasicClient, BasicRequestTokenError},
+    reqwest::{async_http_client, AsyncHttpClientError},
+    url::Url,
+    AuthorizationCode, CsrfToken, Scope, TokenResponse,
+};
+use password_auth::verify_password;
+use serde::{Deserialize, Serialize};
+use sqlx::{FromRow, PgPool};
+use time::OffsetDateTime;
+use uuid::Uuid;
+
+#[derive(Clone, Serialize, Deserialize, FromRow)]
+pub struct User {
+    pub id: Uuid,
+    pub username: String,
+    pub name: String,
+    pub email: String,
+    pub password: Option<String>,
+    pub access_token: Option<String>,
+    pub refresh_token: Option<String>,
+    pub active: bool,
+
+    #[serde(with = "time::serde::iso8601")]
+    pub created_at: OffsetDateTime,
+
+    #[serde(with = "time::serde::iso8601::option")]
+    pub updated_at: Option<OffsetDateTime>,
+
+    #[serde(with = "time::serde::iso8601::option")]
+    pub last_login: Option<OffsetDateTime>,
+}
+
+// Here we've implemented `Debug` manually to avoid accidentally logging the
+// access token.
+impl std::fmt::Debug for User {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("User")
+            .field("id", &self.id)
+            .field("username", &self.username)
+            .field("password", &self.password)
+            .field("email", &self.email)
+            .field("name", &self.name)
+            .field("active", &self.active)
+            .field("created_at", &self.created_at)
+            .field("updated_at", &self.updated_at)
+            .field("last_login", &self.last_login)
+            .field("access_token", &"[redacted]")
+            .field("refresh_token", &"[redacted]")
+            .finish()
+    }
+}
+
+impl AuthUser for User {
+    type Id = Uuid;
+
+    fn id(&self) -> Self::Id {
+        self.id
+    }
+
+    fn session_auth_hash(&self) -> &[u8] {
+        if let Some(access_token) = &self.access_token {
+            return access_token.as_bytes();
+        }
+
+        if let Some(password) = &self.password {
+            return password.as_bytes();
+        }
+
+        &[]
+    }
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub enum Credentials {
+    Password(PasswordCreds),
+    OAuth(OAuthCreds),
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct PasswordCreds {
+    pub username: String,
+    pub password: String,
+    pub next: Option<String>,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct OAuthCreds {
+    pub code: String,
+    pub old_state: CsrfToken,
+    pub new_state: CsrfToken,
+}
+
+#[derive(Debug, Deserialize)]
+struct UserInfo {
+    email: String,
+    name: String,
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum BackendError {
+    #[error(transparent)]
+    Sqlx(sqlx::Error),
+
+    #[error(transparent)]
+    Reqwest(reqwest::Error),
+
+    #[error(transparent)]
+    OAuth2(BasicRequestTokenError<AsyncHttpClientError>),
+}
+
+#[derive(Debug, Clone)]
+pub struct Backend {
+    db: PgPool,
+    client: BasicClient,
+}
+
+impl Backend {
+    pub fn new(db: PgPool, client: BasicClient) -> Self {
+        Self { db, client }
+    }
+
+    pub fn authorize_url(&self) -> (Url, CsrfToken) {
+        let scopes = vec![
+            Scope::new("https://www.googleapis.com/auth/userinfo.email".to_string()),
+            Scope::new("https://www.googleapis.com/auth/userinfo.profile".to_string()),
+        ];
+
+        self.client
+            .authorize_url(CsrfToken::new_random)
+            .add_scopes(scopes.iter().cloned())
+            .url()
+    }
+}
+
+#[async_trait]
+impl AuthnBackend for Backend {
+    type User = User;
+    type Credentials = Credentials;
+    type Error = BackendError;
+
+    async fn authenticate(
+        &self,
+        creds: Self::Credentials,
+    ) -> Result<Option<Self::User>, Self::Error> {
+        match creds {
+            Self::Credentials::Password(password_cred) => {
+                let user: Option<Self::User> = sqlx::query_as(
+                    "select * from users where username = $1 and password is not null",
+                )
+                .bind(password_cred.username)
+                .fetch_optional(&self.db)
+                .await
+                .map_err(Self::Error::Sqlx)?;
+
+                Ok(user.filter(|user| {
+                    // Password will not be `None` given our query above.
+                    let Some(user_password) = &user.password else {
+                        return false;
+                    };
+
+                    verify_password(password_cred.password, user_password)
+                        .ok()
+                        .is_some() // We're using password-based
+                                   // authentication--this
+                                   // works by comparing our form input with an
+                                   // argon2
+                                   // password hash.
+                }))
+            }
+
+            Self::Credentials::OAuth(oauth_creds) => {
+                // Ensure the CSRF state has not been tampered with.
+                if oauth_creds.old_state.secret() != oauth_creds.new_state.secret() {
+                    return Ok(None);
+                };
+
+                // Process authorization code, expecting a token response back.
+                let token_res = self
+                    .client
+                    .exchange_code(AuthorizationCode::new(oauth_creds.code))
+                    .request_async(async_http_client)
+                    .await
+                    .map_err(Self::Error::OAuth2)?;
+
+                // Use access token to request user info.
+                let user_info = reqwest::Client::new()
+                    .get("https://www.googleapis.com/oauth2/v3/userinfo")
+                    .header(USER_AGENT.as_str(), "axum-login")
+                    .header(
+                        AUTHORIZATION.as_str(),
+                        format!("Bearer {}", token_res.access_token().secret()),
+                    )
+                    .send()
+                    .await
+                    .map_err(Self::Error::Reqwest)?
+                    .json::<UserInfo>()
+                    .await
+                    .map_err(Self::Error::Reqwest)?;
+
+                // Persist user in our database so we can use `get_user`.
+                let user = sqlx::query_as(
+                    r#"
+                        insert into users (name, username, email, access_token)
+                        values ($1, $2, $2, $3)
+                        on conflict(username) do update
+                        set access_token = excluded.access_token
+                        returning *
+                    "#,
+                )
+                .bind(user_info.name)
+                .bind(user_info.email)
+                .bind(token_res.access_token().secret())
+                .fetch_one(&self.db)
+                .await
+                .map_err(Self::Error::Sqlx)?;
+
+                Ok(Some(user))
+            }
+        }
+    }
+
+    async fn get_user(&self, user_id: &UserId<Self>) -> Result<Option<Self::User>, Self::Error> {
+        Ok(sqlx::query_as("select * from users where id = $1")
+            .bind(user_id)
+            .fetch_optional(&self.db)
+            .await
+            .map_err(Self::Error::Sqlx)?)
+    }
+}
+
+// We use a type alias for convenience.
+//
+// Note that we've supplied our concrete backend here.
+pub type AuthSession = axum_login::AuthSession<Backend>;
---
+++ src/web/app.rs
@@ -0,0 +1,95 @@
+use std::env;
+
+use axum_login::{
+    login_required,
+    tower_sessions::{cookie::SameSite, Expiry, SessionManagerLayer},
+    AuthManagerLayerBuilder,
+};
+use oauth2::{basic::BasicClient, AuthUrl, ClientId, ClientSecret, RedirectUrl, TokenUrl};
+use sqlx::{postgres::PgPoolOptions, PgPool};
+use time::Duration;
+use tower_sessions_redis_store::{fred::prelude::*, RedisStore};
+
+use crate::{
+    users::Backend,
+    web::{auth, oauth, protected},
+};
+
+pub struct App {
+    db: PgPool,
+    client: BasicClient,
+}
+
+impl App {
+    pub async fn new() -> Result<Self, Box<dyn std::error::Error>> {
+        dotenvy::dotenv()?;
+
+        //////////////////////////////////////////////////////////////////////////////////
+        //////////////////////////////// OAuth ///////////////////////////////////////////
+        //////////////////////////////////////////////////////////////////////////////////
+        let client_id = env::var("CLIENT_ID")
+            .map(ClientId::new)
+            .expect("CLIENT_ID should be provided.");
+        let client_secret = env::var("CLIENT_SECRET")
+            .map(ClientSecret::new)
+            .expect("CLIENT_SECRET should be provided");
+
+        let auth_url = AuthUrl::new("https://accounts.google.com/o/oauth2/v2/auth".to_string())?;
+        let token_url = TokenUrl::new("https://www.googleapis.com/oauth2/v3/token".to_string())?;
+
+        let client = BasicClient::new(client_id, Some(client_secret), auth_url, Some(token_url))
+            .set_redirect_uri(
+                RedirectUrl::new("http://localhost:3000/oauth/callback".to_string())
+                    .expect("Invalid redirect URL"),
+            );
+
+        //////////////////////////////////////////////////////////////////////////////////
+        /////////////////////////////////  DB  ///////////////////////////////////////////
+        //////////////////////////////////////////////////////////////////////////////////
+        let db_url: String = env::var("DATABASE_URL").unwrap();
+        let db = PgPoolOptions::new()
+            .max_connections(5)
+            .connect(&db_url)
+            .await
+            .expect("error connection to db");
+
+        sqlx::migrate!().run(&db).await?;
+
+        Ok(Self { db, client })
+    }
+
+    pub async fn serve(self) -> Result<(), Box<dyn std::error::Error>> {
+        //////////////////////////////////////////////////////////////////////////////////
+        /////////////////////////////////  REDIS  ////////////////////////////////////////
+        //////////////////////////////////////////////////////////////////////////////////
+        // Session layer.
+        //
+        // This uses `tower-sessions` to establish a layer that will provide the session
+        // as a request extension.
+        let pool = RedisPool::new(RedisConfig::default(), None, None, None, 6).unwrap();
+        pool.wait_for_connect().await.unwrap();
+        let session_store = RedisStore::new(pool);
+        let session_layer = SessionManagerLayer::new(session_store)
+            .with_secure(false)
+            .with_same_site(SameSite::Lax) // Ensure we send the cookie from the OAuth redirect.
+            .with_expiry(Expiry::OnInactivity(Duration::days(1)));
+
+        // Auth service.
+        //
+        // This combines the session layer with our backend to establish the auth
+        // service which will provide the auth session as a request extension.
+        let backend = Backend::new(self.db, self.client);
+        let auth_layer = AuthManagerLayerBuilder::new(backend, session_layer).build();
+
+        let app = protected::router()
+            .route_layer(login_required!(Backend, login_url = "/login"))
+            .merge(auth::router())
+            .merge(oauth::router())
+            .layer(auth_layer);
+
+        let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+        axum::serve(listener, app.into_make_service()).await?;
+
+        Ok(())
+    }
+}
